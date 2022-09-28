c81bf983835fffc25235989c1534aa85d6cacf8e
rpkrupali1 <rpkrupali@gmail.com>
2022-09-28 13:22:55 -0500

    Added insertion sort tests

--- src/main/java/sort/Insertion.java
+++ src/main/java/sort/Insertion.java
@@ -2,7 +2,14 @@ package sort;
 
 import java.util.ArrayList;
 
-public class insertion {
+/**
+ * Insertion sort:
+ * Works similar to the way you sort playing cards
+ * Array is split into sorted and unsorted array. Values from unsorted part are picked and placed at correct position in sorted array
+ * Design Strategy - Decrease and conquer
+ * Efficient for - small data sets, already partially sorted arrays
+ */
+public class Insertion {
     public ArrayList<Integer> insertion_sort(ArrayList<Integer> arr){
         int n = arr.size();
         for (int i = 0; i < n ; i++) {
