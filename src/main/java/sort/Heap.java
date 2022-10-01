9408a88428c20ff81a9562351dc27e51ab8b9ea9
rpkrupali1 <rpkrupali@gmail.com>
2022-10-01 11:22:44 -0500

    heap sort algorithm and tests

---
+++ src/main/java/sort/Heap.java
@@ -0,0 +1,64 @@
+package sort;
+
+import java.util.ArrayList;
+
+/*
+Heap sort is a comparison based sorting technique based on Binary Heap data structure.
+It is similar to selection sort where we first find the maximum element and place the maximum element at the end.
+We repeat the same process for remaining element.
+Heap sort is in-place algorithm.
+Typical implementation is not stable but can be made stable
+Slower than Quick sort or Merge sort
+Steps:
+1) Heapify
+2) One by one delete the root node and replace with last leaf node and then heapify
+3) Repeat the process
+*/
+
+public class Heap {
+    public ArrayList<Integer> heap_sort(ArrayList<Integer> arr){
+        int n = arr.size();
+        //build heap, you only need to consider internal nodes to heapify
+        for (int i = n/2-1; i >=0; i--) {
+            heapify(arr,n,i);
+        }
+
+        //one by one delete the root and replace with last node
+        for (int i = n-1; i >0 ; i--) {
+            //move current root to end
+            int root = arr.get(0);
+            arr.set(0,arr.get(i));
+            arr.set(i,root);
+
+            //call heapify again
+            heapify(arr,i,0);
+        }
+        return arr;
+    }
+
+    // To heapify a subtree rooted with node i which is
+    // a root index in arr[]. n is size of heap
+    private void heapify(ArrayList<Integer>arr, int n, int i) {
+        int largest = i; //initialize largest as root
+        int leftIndex = 2 * i + 1; //as per heapify formula, leftIndex = 2*i + 1
+        int rightIndex = 2 * i + 2; // as per heapify formula, rightIndex = 2*i + 2
+
+        //compare between left child and parent(root)
+        if (leftIndex < n && arr.get(leftIndex) > arr.get(largest) )
+            largest = leftIndex;
+
+        //compare right with largest
+        if (rightIndex < n && arr.get(rightIndex) > arr.get(largest))
+            largest = rightIndex;
+
+        //if root is not the largest then heapify
+        if (largest != i) {
+            int temp = arr.get(i);
+            arr.set(i, arr.get(largest));
+            arr.set(largest, temp);
+
+            //recursively heapify the subtrees
+            heapify(arr, n, largest);
+        }
+    }
+}
---
+++ src/test/java/sort/HeapSortTest.java
@@ -0,0 +1,119 @@
+package sort;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class HeapSortTest extends ParentTest {
+
+    @Test
+    public void test000(){
+        Heap sort = new Heap();
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(5, 8, 3, 9, 4, 1, 7));
+        ArrayList<Integer> output = sort.heap_sort(input);
+        printTest(output);
+        Assert.assertTrue(isSorted(output));
+    }
+
+    @Test
+    public void test001() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(0, 199999999, 1000000000));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test002() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test003() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1, -2));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test004() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1000000000, 0));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test005() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(-9, 9));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test006() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(0, 3, 2, 1));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test007() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1, 1, 1, -1));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test008() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 2, 1));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test009() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(-1, -2, -3, -4));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test010() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1, 3, 5, 7, -2, -4, -6));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+
+    @Test
+    public void test011() {
+        Heap heap = new Heap();
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(1, 2, 1, 2));
+        ArrayList<Integer> output = heap.heap_sort(input);
+        printTest(output);
+        isSorted(output);
+    }
+}
