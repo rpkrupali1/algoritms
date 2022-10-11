480d4c70b689e570f5a5f666caf31e61b9ba9663
rpkrupali1 <rpkrupali@gmail.com>
2022-10-11 07:48:43 -0500

    insertion sort documentation

--- src/main/java/sort/Insertion.java
+++ src/main/java/sort/Insertion.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public class Insertion {
     public ArrayList<Integer> insertion_sort(ArrayList<Integer> arr){
         int n = arr.size();
-        for (int i = 0; i < n ; i++) {
+        for (int i = 1; i < n ; i++) {
             int temp = arr.get(i);
             int j = i-1;
             while (j>=0 && arr.get(j)>temp){
