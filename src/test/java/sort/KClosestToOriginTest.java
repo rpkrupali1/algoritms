8f9562be86196994148df22b250d6725e5664017
rpkrupali1 <rpkrupali@gmail.com>
2022-10-22 06:30:16 -0500

    kth closest to origin

---
+++ src/test/java/sort/KClosestToOriginTest.java
@@ -0,0 +1,29 @@
+package sort;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import sort.examples.KClosestToOrigin;
+
+public class KClosestToOriginTest extends ParentTest{
+
+    @Test
+    public void test000(){
+        int[][] input = {{1, 3}, {-2, 2}};
+        int k = 1;
+        KClosestToOrigin sol = new KClosestToOrigin();
+        int[][] actualOutput = sol.kClosest(input,k);
+        int[][] expectedOutput =  {{-2, 2}};
+        Assert.assertEquals(actualOutput,expectedOutput);
+    }
+
+    @Test
+    public void test001(){
+        //int[][] input = {{3,3},{5,-1},{-2,4}};
+        int[][] input = {{5,-1},{3,3},{-2,4}};
+        int k = 2;
+        KClosestToOrigin sol = new KClosestToOrigin();
+        int[][] actualOutput = sol.kClosest(input,k);
+        int[][] expectedOutput =  {{-2,4},{3,3}};
+        Assert.assertEquals(actualOutput,expectedOutput);
+    }
+}
