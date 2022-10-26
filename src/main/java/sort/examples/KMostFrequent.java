b0ac0ac9a1adcea3a2632cae4dc24eaffcd8667c
rpkrupali1 <rpkrupali@gmail.com>
2022-10-26 16:09:45 -0500

    k most frequent number

--- src/main/java/sort/examples/KMostFrequent.java
+++ src/main/java/sort/examples/KMostFrequent.java
@@ -13,9 +13,29 @@ import java.util.*;
  *
  * Output:
  * ["car", "driver", "taxi", "bus"]
+ * Problem
+ *
+ * Top K Frequent Elements
+ * Given an integer array and a number k, find the k most frequent elements in the array.
+ *
+ * Example One
+ * {
+ * "arr": [1, 2, 3, 2, 4, 3, 1],
+ * "k": 2
+ * }
+ * Output:
+ * [3, 1]
+ *
+ * Example Two
+ * {
+ * "arr": [1, 2, 1, 2, 3, 1],
+ * "k": 1
+ * }
+ * Output:
+ * [1]
  */
 public class KMostFrequent {
-    public ArrayList<String> k_most_frequent(Integer k, ArrayList<String> words) {
+    public ArrayList<String> kMostFrequent_words_pq(Integer k, ArrayList<String> words) {
         Map<String,Integer> word_count = new HashMap<>();
         //put count of all distinct words into map
         for (String word : words) {
@@ -40,6 +60,53 @@ public class KMostFrequent {
         }
 
         return result;
+    }
+
+    /**
+     * Heap solution
+     * Time - O(N logK)
+     *  Space - O(N+k)
+     */
+    public ArrayList<Integer> kMostFrequentElements_pq(ArrayList<Integer> arr,int k){
+        if(k == arr.size())
+            return arr;
+        ArrayList<Integer> result = new ArrayList<>();
+        //calculate and store frequency of each element into map
+        HashMap<Integer,Integer>map = new HashMap<>();
+
+        for (Integer number:arr) {
+            map.put(number, map.getOrDefault(number,0)+1);
+        }
+
+        //order by frequency using priority queue
+        PriorityQueue<pair> pq = new PriorityQueue<>();
+
+        //add it to queue
+        for (Integer number: map.keySet() ) {
+            int count = map.get(number);
+            pq.add(new pair(number,count));
+            //remove unwanted
+            if(pq.size()>k)
+                pq.poll();
+        }
+
+        for (int i = 0; i < k; i++) {
+            result.add(pq.poll().element);
+        }
+        return result;
+    }
+}
+
+class pair implements Comparable<pair> {
+    int element, count;
+    public pair(int element, int count){
+        this.element = element;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(pair o){
+        return this.count - o.count;
     }
 }
 
--- src/test/java/sort/KMostFrequentTest.java
+++ src/test/java/sort/KMostFrequentTest.java
@@ -12,7 +12,7 @@ public class KMostFrequentTest extends ParentTest{
     public void test000(){
         ArrayList<String> input = new ArrayList<>(Arrays.asList("car", "bus", "taxi", "car", "driver", "candy", "race", "car", "driver", "fare", "taxi"));
         KMostFrequent kMostFrequent = new KMostFrequent();
-        ArrayList<String> actualOutput = kMostFrequent.k_most_frequent(4,input);
+        ArrayList<String> actualOutput = kMostFrequent.kMostFrequent_words_pq(4,input);
         ArrayList<String> expectedOutput =new ArrayList<>(Arrays.asList("car", "driver", "taxi", "bus"));
         Assert.assertEquals(actualOutput,expectedOutput);
     }
@@ -21,7 +21,7 @@ public class KMostFrequentTest extends ParentTest{
     public void test001(){
         ArrayList<String> input = new ArrayList<>(Arrays.asList("car", "car", "car", "car", "car", "car"));
         KMostFrequent kMostFrequent = new KMostFrequent();
-        ArrayList<String> actualOutput = kMostFrequent.k_most_frequent(1,input);
+        ArrayList<String> actualOutput = kMostFrequent.kMostFrequent_words_pq(1,input);
         ArrayList<String> expectedOutput =new ArrayList<>(Arrays.asList("car"));
         Assert.assertEquals(actualOutput,expectedOutput);
     }
@@ -30,8 +30,28 @@ public class KMostFrequentTest extends ParentTest{
     public void test002(){
         ArrayList<String> input = new ArrayList<>(Arrays.asList("car", "car", "taxi", "taxi", "motor", "motor", "driver", "driver", "life", "life"));
         KMostFrequent kMostFrequent = new KMostFrequent();
-        ArrayList<String> actualOutput = kMostFrequent.k_most_frequent(5,input);
+        ArrayList<String> actualOutput = kMostFrequent.kMostFrequent_words_pq(5,input);
         ArrayList<String> expectedOutput =new ArrayList<>(Arrays.asList("car", "driver", "life", "motor", "taxi"));
+        Assert.assertEquals(actualOutput,expectedOutput);
+    }
+
+    @Test
+    public void numbers_test000(){
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3, 2, 4, 3, 1));
+        KMostFrequent kMostFrequent = new KMostFrequent();
+        ArrayList<Integer> actualOutput = kMostFrequent.kMostFrequentElements_pq(input,2);
+        printTest(actualOutput);
+        ArrayList<Integer> expectedOutput =new ArrayList<>(Arrays.asList(3,2));
+        Assert.assertEquals(actualOutput,expectedOutput);
+    }
+
+    @Test
+    public void numbers_test001(){
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 1, 2, 3, 1));
+        KMostFrequent kMostFrequent = new KMostFrequent();
+        ArrayList<Integer> actualOutput = kMostFrequent.kMostFrequentElements_pq(input,1);
+        printTest(actualOutput);
+        ArrayList<Integer> expectedOutput =new ArrayList<>(Arrays.asList(1));
         Assert.assertEquals(actualOutput,expectedOutput);
     }
 }
