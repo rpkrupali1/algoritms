1b09cad3dff0f33b0c5db3372b0bbe4a4372f839
rpkrupali1 <rpkrupali@gmail.com>
2022-10-27 12:44:46 -0500

    kth largest with Arraylist input

--- src/test/java/sort/KthLargestElementTest.java
+++ src/test/java/sort/KthLargestElementTest.java
@@ -4,6 +4,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import sort.examples.KthLargest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class KthLargestElementTest extends ParentTest {
 
     @Test
@@ -26,7 +29,7 @@ public class KthLargestElementTest extends ParentTest {
     public void pq_test000(){
         int[] input = {3,2,1,5,6,4};
         KthLargest kth = new KthLargest();
-        int actualOutput = kth.getkth_priorityQueue(input,2);
+        int actualOutput = kth.priorityQueue(input,2);
         Assert.assertEquals(actualOutput,5);
     }
 
@@ -34,7 +37,23 @@ public class KthLargestElementTest extends ParentTest {
     public void pq_test001(){
         int[] input = {3,2,1,3,1,2,4,5,5,6};
         KthLargest kth = new KthLargest();
-        int actualOutput = kth.getkth_priorityQueue(input,4);
+        int actualOutput = kth.priorityQueue(input,4);
+        Assert.assertEquals(actualOutput,4);
+    }
+
+    @Test
+    public void pq2_test000(){
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(3,2,1,5,6,4));
+        KthLargest kth = new KthLargest();
+        int actualOutput = kth.priorityQueue(input,2);
+        Assert.assertEquals(actualOutput,5);
+    }
+
+    @Test
+    public void pq2_test001(){
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(3,2,1,3,1,2,4,5,5,6));
+        KthLargest kth = new KthLargest();
+        int actualOutput = kth.priorityQueue(input,4);
         Assert.assertEquals(actualOutput,4);
     }
 }
