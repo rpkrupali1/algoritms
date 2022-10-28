9ac182b9ec6bfe219b675fbdebdb21c1327861a2
rpkrupali1 <rpkrupali@gmail.com>
2022-10-28 05:40:52 -0500

    group the number or polish national flag

---
+++ src/test/java/sort/GroupTheNumbersTest.java
@@ -0,0 +1,31 @@
+package sort;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import sort.examples.GroupTheNumbers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GroupTheNumbersTest extends ParentTest{
+
+    @Test
+    public void test000(){
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1,2,3,4));
+        GroupTheNumbers gp = new GroupTheNumbers();
+        ArrayList<Integer> actualOutput = gp.twoPointer(input);
+        printTest(actualOutput);
+        ArrayList<Integer> expectedOutput = new ArrayList<>(Arrays.asList(2,4,3,1));
+        Assert.assertEquals(actualOutput,expectedOutput);
+    }
+
+    @Test
+    public void test001(){
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1,5,8,7));
+        GroupTheNumbers gp = new GroupTheNumbers();
+        ArrayList<Integer> actualOutput = gp.twoPointer(input);
+        printTest(actualOutput);
+        ArrayList<Integer> expectedOutput = new ArrayList<>(Arrays.asList(8,5,1,7));
+        Assert.assertEquals(actualOutput,expectedOutput);
+    }
+}
