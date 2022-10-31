45a2140a554e586fbfdd763c05761b3e1168a4ac
rpkrupali1 <rpkrupali@gmail.com>
2022-10-31 17:19:47 -0500

    fix recursion - n choose k combination

--- src/test/java/recurssion/NChooseKCombinationsTest.java
+++ src/test/java/recurssion/NChooseKCombinationsTest.java
@@ -24,8 +24,15 @@ public class NChooseKCombinationsTest extends ParentTest {
         expected.add( new ArrayList<>(Arrays.asList(3, 4)));
         expected.add( new ArrayList<>(Arrays.asList(3, 5)));
         expected.add( new ArrayList<>(Arrays.asList(4, 5)));
-
         Assert.assertEquals(actual,expected);
+    }
 
+    @Test
+    public static void recurive_test001(){
+        ArrayList<ArrayList<Integer>> actual = NChooseKCombinations.recursive(6,6);
+        printList(actual);
+        ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+        expected.add( new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+        Assert.assertEquals(actual,expected);
     }
 }
