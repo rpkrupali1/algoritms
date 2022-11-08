f2aeb680ad8c23f03b3ce025deb9918f3ed8e994
rpkrupali <rpkrupali@gmail.com>
2022-11-08 07:58:46 -0600

    words from phone numbers

---
+++ src/test/java/recurssion/WordsFromPhoneNumberTest.java
@@ -0,0 +1,28 @@
+package recurssion;
+
+import common.ParentTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WordsFromPhoneNumberTest extends ParentTest {
+
+    @Test
+    public void test000(){
+        ArrayList<String> actual = WordsFromPhoneNumber.recursion("23");
+        printStrings(actual);
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("ad", "ae","af","bd","be","bf","cd","ce","cf"));
+        Assert.assertEquals(actual,expected);
+    }
+
+
+    @Test
+    public void test001(){
+        ArrayList<String> actual = WordsFromPhoneNumber.recursion("01");
+        printStrings(actual);
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList(""));
+        Assert.assertEquals(actual,expected);
+    }
+}
