c617e4565febb4a1f01a9350b9fc8d18ea3e7f24
rpkrupali <rpkrupali@gmail.com>
2022-11-09 05:51:53 -0600

    power of a large number

--- src/main/java/recurssion/PowerOfNumber.java
+++ src/main/java/recurssion/PowerOfNumber.java
@@ -1,15 +1,30 @@
 package recurssion;
 
 /**
- * Write a Java program to calculate the power of a number like power(int number, int power) like power(2, 3) should return 8
+ * Power
+ * Given a base a and an exponent b. Your task is to find ab. The value could be large enough. So, calculate ab % 1000000007.
+ * Example
+ * input: "a": 2,  "b": 10
+ * Output: 1024
  */
 public class PowerOfNumber {
-    public static int recursive(int n, int p){
-        //System.out.println(p);
-//        if(p==0)
-//            return 1;
-        if(p==1 || n==1 || n==0)
+    private static long mod = 1000000007;
+    public static long mod_approach(long n, long p){
+        if( n==1 || n==0)
             return n;
-        return n * recursive(n,p-1);
+        if(p==0)
+            return 1;
+        n= n%mod;
+        return (n * mod_approach(n,p-1))%mod;
+    }
+
+    public static long divide_conquer(long n , long p){
+        if( n==1 || n==0)
+            return n;
+        if(p==1)
+            return n%mod;
+        long temp = divide_conquer(n,p/2);
+        if(p%2==0) return (temp%mod)*(temp%mod)%mod;
+        else return ((((temp%mod)*(temp%mod))%mod)*(n%mod))%mod;
     }
 }
--- src/test/java/recurssion/PowerOfNumberTest.java
+++ src/test/java/recurssion/PowerOfNumberTest.java
@@ -7,31 +7,55 @@ public class PowerOfNumberTest {
 
     @Test
     public void test000(){
-        int actual = PowerOfNumber.recursive(2,3);
+        long actual = PowerOfNumber.mod_approach(2,3);
         Assert.assertEquals(actual,8);
     }
 
     @Test
     public void test001(){
-        int actual = PowerOfNumber.recursive(3,4);
+        long actual = PowerOfNumber.mod_approach(3,4);
         Assert.assertEquals(actual,81);
     }
 
     @Test
     public void test002(){
-        int actual = PowerOfNumber.recursive(1, 10000000);
+        long actual = PowerOfNumber.mod_approach(1, 10000000);
         Assert.assertEquals(actual,1);
     }
 
     @Test
     public void test003(){
-        int actual = PowerOfNumber.recursive(10000, 10000000);
+        long actual = PowerOfNumber.mod_approach(10000, 10000000);
         Assert.assertEquals(actual,144659229);
     }
 
     @Test
     public void test004(){
-        int actual = PowerOfNumber.recursive(2, 30);
-        Assert.assertEquals(actual,1073741824);
+        long actual = PowerOfNumber.mod_approach(2, 32);
+        Assert.assertEquals(actual,294967268);
+    }
+
+    @Test
+    public void divide_conquer_test001(){
+        long actual = PowerOfNumber.divide_conquer(3,4);
+        Assert.assertEquals(actual,81);
+    }
+
+    @Test
+    public void divide_conquer_test002(){
+        long actual = PowerOfNumber.divide_conquer(1, 10000000);
+        Assert.assertEquals(actual,1);
+    }
+
+    @Test
+    public void divide_conquer_test003(){
+        long actual = PowerOfNumber.divide_conquer(10000, 10000000);
+        Assert.assertEquals(actual,144659229);
+    }
+
+    @Test
+    public void divide_conquer_test004(){
+        long actual = PowerOfNumber.divide_conquer(2, 32);
+        Assert.assertEquals(actual,294967268);
     }
 }
