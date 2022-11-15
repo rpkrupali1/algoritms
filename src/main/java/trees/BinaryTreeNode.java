7ce10b7f024b03adae9e1aacee611d91039eab2d
rpkrupali <rpkrupali@gmail.com>
2022-11-15 04:43:39 -0600

    Binary tree node moved to root of trees

---
+++ src/main/java/trees/BinaryTreeNode.java
@@ -0,0 +1,13 @@
+package trees;
+
+class BinaryTreeNode {
+    Integer value;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+    BinaryTreeNode(Integer value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
