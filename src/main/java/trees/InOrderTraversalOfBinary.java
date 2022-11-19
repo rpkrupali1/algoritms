a4ee8a322d837fc0fc75de7c9a031e0ae13a36f1
rpkrupali <rpkrupali@gmail.com>
2022-11-19 06:25:34 -0600

    in order traversal of binary tree

--- src/main/java/trees/InOrderTraversalOfBinary.java
+++ src/main/java/trees/InOrderTraversalOfBinary.java
@@ -1,2 +1,27 @@
-package trees;public class InOrderTraversalOfBinary {
+package trees;
+
+import java.util.ArrayList;
+
+/**
+ * Inorder Traversal Of A Binary Tree
+ * Given a binary tree, return the inorder traversal of its node values.
+ * Example One :  0/1\2 /3\4 /null\null
+ * Output: [3, 1, 4, 0, 2]
+ * Example Two : 0/1\null null\2 3/null
+ * Output: [1, 3, 2, 0]
+ */
+public class InOrderTraversalOfBinary {
+    public static ArrayList<Integer> inOrder(BinaryTreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        helper(root,result);
+        return result;
+    }
+
+    private static void helper(BinaryTreeNode root,ArrayList<Integer> result){
+        if(root==null)
+            return;
+        helper(root.left,result);
+        result.add(root.value);
+        helper(root.right,result);
+    }
 }
--- src/test/java/trees/InOrderTraversalOfBinaryTest.java
+++ src/test/java/trees/InOrderTraversalOfBinaryTest.java
@@ -1,2 +1,35 @@
-package trees;public class InOrderTraversalOfBinaryTest {
+package trees;
+
+import common.ParentTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InOrderTraversalOfBinaryTest extends ParentTest {
+    @Test
+    public void test000(){
+        BinaryTreeNode btn = new BinaryTreeNode(0);
+        btn.left = new BinaryTreeNode(1);
+        btn.right = new BinaryTreeNode(2);
+        btn.left.left = new BinaryTreeNode(3);
+        btn.left.right = new BinaryTreeNode(4);
+        ArrayList<Integer> actual = InOrderTraversalOfBinary.inOrder(btn);
+        print(actual);
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(3, 1, 4, 0, 2));
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test001(){
+        BinaryTreeNode btn = new BinaryTreeNode(0);
+        btn.left = new BinaryTreeNode(1);
+        btn.left.right = new BinaryTreeNode(2);
+        btn.left.right.left = new BinaryTreeNode(3);
+        ArrayList<Integer> actual = InOrderTraversalOfBinary.inOrder(btn);
+        print(actual);
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(1, 3, 2, 0));
+        Assert.assertEquals(actual,expected);
+    }
 }
