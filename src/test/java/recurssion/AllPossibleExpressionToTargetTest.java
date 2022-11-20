1857c01e945347a4ee38de2b87e9a901d5c397fd
rpkrupali <rpkrupali@gmail.com>
2022-11-20 05:37:11 -0600

    updates to all possible expression to target test

--- src/test/java/recurssion/AllPossibleExpressionToTargetTest.java
+++ src/test/java/recurssion/AllPossibleExpressionToTargetTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class AllPossibleExpressionToTargetTest extends ParentTest {
 
@@ -14,6 +15,38 @@ public class AllPossibleExpressionToTargetTest extends ParentTest {
         ArrayList<String> actual = AllPossibleExpressionToTarget.generate_all_expressions("202",4);
         printStrings(actual);
         ArrayList<String> expected = new ArrayList<>(Arrays.asList("2+0+2", "2*02", "2+02"));
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test001(){
+        ArrayList<String> actual = AllPossibleExpressionToTarget.generate_all_expressions("1234",11);
+        printStrings(actual);
+        ArrayList<String> expected = new ArrayList<>(List.of("1+2*3+4"));
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test002(){
+        ArrayList<String> actual = AllPossibleExpressionToTarget.generate_all_expressions("99",1);
+        printStrings(actual);
+        ArrayList<String> expected = new ArrayList<>();
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test003(){
+        ArrayList<String> actual = AllPossibleExpressionToTarget.generate_all_expressions("0",0);
+        printStrings(actual);
+        ArrayList<String> expected = new ArrayList<>(List.of("0"));
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test006(){
+        ArrayList<String> actual = AllPossibleExpressionToTarget.generate_all_expressions("22",4);
+        printStrings(actual);
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("2*2", "2+2"));
         Assert.assertEquals(actual,expected);
     }
 }
