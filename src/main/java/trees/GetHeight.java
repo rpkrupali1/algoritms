c3dd87cea503d51c506bd68a47791a6895d1ed6d
rpkrupali <rpkrupali@gmail.com>
2022-11-20 06:47:11 -0600

    get max height if a tree

--- src/main/java/trees/GetHeight.java
+++ src/main/java/trees/GetHeight.java
@@ -4,6 +4,7 @@ package trees;
  * The height of a binary search tree is the number of edges between the tree's root and its furthest leaf.
  * You are given a pointer pointing to the root of a binary search tree.
  * Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
+ * Get maximum height of a given tree
  */
 public class GetHeight {
     public static int getHeight(BinaryTreeNode root){
