3a516ecbc9aef4f6efeaaf1ecde5ef60ac0d256b
rpkrupali <rpkrupali@gmail.com>
2022-11-26 17:26:17 -0600

    all path of tree

--- src/test/java/trees/AllPathsOfTreeTest.java
+++ src/test/java/trees/AllPathsOfTreeTest.java
@@ -23,4 +23,23 @@ public class AllPathsOfTreeTest extends ParentTest {
         expected.add(new ArrayList<>(List.of(10, 30)));
         Assert.assertEquals(actual,expected);
     }
+
+    @Test
+    public void test001(){
+        BinaryTreeNode input = new BinaryTreeNode(1);
+        input.left = new BinaryTreeNode(2);
+        input.right = new BinaryTreeNode(3);
+        input.left.left = new BinaryTreeNode(4);
+        input.left.right = new BinaryTreeNode(5);
+        input.right.left = new BinaryTreeNode(6);
+        input.right.right = new BinaryTreeNode(7);
+        ArrayList<ArrayList<Integer>> actual = AllPathsOfTree.dfs(input);
+        printList(actual);
+        ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(List.of(1, 2, 4)));
+        expected.add(new ArrayList<>(List.of(1, 2, 5)));
+        expected.add(new ArrayList<>(List.of(1, 3,6)));
+        expected.add(new ArrayList<>(List.of(1, 3,7)));
+        Assert.assertEquals(actual,expected);
+    }
 }
