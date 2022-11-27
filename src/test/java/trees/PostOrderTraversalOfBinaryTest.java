d16020f8cf3c65212c325f2e9c3da01798e3521e
rpkrupali <rpkrupali@gmail.com>
2022-11-27 04:19:24 -0600

    post order traversal without recursion

--- src/test/java/trees/PostOrderTraversalOfBinaryTest.java
+++ src/test/java/trees/PostOrderTraversalOfBinaryTest.java
@@ -9,27 +9,40 @@ import java.util.Arrays;
 
 public class PostOrderTraversalOfBinaryTest extends ParentTest {
     @Test
-    public void test000(){
+    public void test000_recursion(){
         BinaryTreeNode btn = new BinaryTreeNode(0);
         btn.left = new BinaryTreeNode(1);
         btn.right = new BinaryTreeNode(2);
         btn.left.left = new BinaryTreeNode(3);
         btn.left.right = new BinaryTreeNode(4);
-        ArrayList<Integer> actual = PostOrderTraversalOfBinary.postOrder(btn);
+        ArrayList<Integer> actual = PostOrderTraversalOfBinary.recursion(btn);
         print(actual);
         ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(3, 4, 1, 2, 0));
         Assert.assertEquals(actual,expected);
     }
 
     @Test
-    public void test001(){
+    public void test001_recursion(){
         BinaryTreeNode btn = new BinaryTreeNode(0);
         btn.left = new BinaryTreeNode(1);
         btn.left.right = new BinaryTreeNode(2);
         btn.left.right.left = new BinaryTreeNode(3);
-        ArrayList<Integer> actual = PostOrderTraversalOfBinary.postOrder(btn);
+        ArrayList<Integer> actual = PostOrderTraversalOfBinary.recursion(btn);
         print(actual);
         ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(3, 2, 1, 0));
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test000_withoutTreeModification(){
+        BinaryTreeNode btn = new BinaryTreeNode(0);
+        btn.left = new BinaryTreeNode(1);
+        btn.right = new BinaryTreeNode(2);
+        btn.left.left = new BinaryTreeNode(3);
+        btn.left.right = new BinaryTreeNode(4);
+        ArrayList<Integer> actual = PostOrderTraversalOfBinary.withoutTreeModification(btn);
+        print(actual);
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(3, 4, 1, 2, 0));
         Assert.assertEquals(actual,expected);
     }
 }
