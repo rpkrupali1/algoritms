6112e65ba26990992a0605e74505b80c3227bab0
rpkrupali <rpkrupali@gmail.com>
2022-12-06 05:59:05 -0600

    count islands

---
+++ src/test/java/graphs/CountIslandsTest.java
@@ -0,0 +1,23 @@
+package graphs;
+
+import common.ParentTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountIslandsTest extends ParentTest {
+
+    @Test
+    public void test000(){
+        ArrayList<ArrayList<Integer>> input = new ArrayList<>();
+        input.add(new ArrayList<>(List.of(1, 1, 0, 0, 0)));
+        input.add(new ArrayList<>(List.of(0, 1, 0, 0, 1)));
+        input.add(new ArrayList<>(List.of(1, 0, 0, 1, 1)));
+        input.add(new ArrayList<>(List.of(0, 0, 0, 0, 0)));
+        input.add(new ArrayList<>(List.of(1, 0, 1, 0, 1)));
+        int actual = CountIslands.bfs(input);
+        Assert.assertEquals(actual,5);
+    }
+}
