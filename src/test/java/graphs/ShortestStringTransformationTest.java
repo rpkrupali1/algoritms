41de760c42f690c3c0f2da98968c3a157c148e45
rpkrupali <rpkrupali@gmail.com>
2022-12-11 06:16:52 -0600

    shprtest string transformation

---
+++ src/test/java/graphs/ShortestStringTransformationTest.java
@@ -0,0 +1,42 @@
+package graphs;
+
+import common.ParentTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShortestStringTransformationTest extends ParentTest {
+
+    @Test
+    public void test000_dict(){
+        ArrayList<String> words = new ArrayList<>(List.of("cat", "hat", "bad", "had"));
+        ArrayList<String> actual = ShortestStringTransformation.usingDictionary(words,"bat","had");
+        printStrings(actual);
+        ArrayList<ArrayList<String>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(List.of("bat", "hat", "had")));
+        expected.add(new ArrayList<>(List.of("bat", "bad", "had")));
+        Assert.assertTrue(expected.contains(actual));
+    }
+
+    @Test
+    public void test001_dict(){
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> actual = ShortestStringTransformation.usingDictionary(words,"bbb","bbc");
+        printStrings(actual);
+        ArrayList<ArrayList<String>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(List.of("bbb", "bbc")));
+        Assert.assertTrue(expected.contains(actual));
+    }
+
+    @Test
+    public void test002_dict(){
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> actual = ShortestStringTransformation.usingDictionary(words,"zzzzzz","zzzzzz");
+        printStrings(actual);
+        ArrayList<ArrayList<String>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(List.of("-1")));
+        Assert.assertTrue(expected.contains(actual));
+    }
+}
